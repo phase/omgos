01cc76d603957247ed1a243b76e8df2175c7e7f0
Jadon Fowler <jadonflower@gmail.com>
2016-05-16 18:27:47 -0700

    Read config.json and run commands in it
    config.json is loaded into a map[string]interface{}, and command is
    grabbed through the file extension.
    
    Signed-off-by: Jadon Fowler <jadonflower@gmail.com>

--- omgos.go
+++ omgos.go
@@ -1,22 +1,65 @@
 package main
 
 import (
+    "os"
     "fmt"
+    "strings"
+    "os/exec"
     "net/http"
     "io/ioutil"
+    "encoding/json"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Printf("GET %s\n", r.URL.Path[1:])
-    data, err := ioutil.ReadFile(r.URL.Path[1:])
+var config map[string]interface{}
+
+func readConfig() {
+    data, err := ioutil.ReadFile("config.json")
     if err != nil {
-        fmt.Fprintf(w, "%q\n", err)
+        fmt.Println("  Error:", err);
     } else {
-        w.Write(data)
+        var c map[string]interface{}
+        if err = json.Unmarshal(data, &c); err != nil {
+            fmt.Println("  Error:", err);
+            return
+        }
+        config = c
+    }
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    path := r.URL.Path[1:]
+    fmt.Printf("GET %s\n", path)
+    
+    //Get file extension
+    s := strings.Split(path, ".")
+    extension := s[len(s) - 1]
+    
+    //TODO: Clean up code
+    if(config[extension] != nil) {
+        command := config[extension].(string)
+        pwd, _ := os.Getwd()
+        command = strings.Replace(command, "$file", pwd + "/" + path, -1)
+        fmt.Println(" Running command:", command)
+        splitCommand := strings.Split(command, " ")
+        data, err := exec.Command(splitCommand[0], splitCommand[1:]...).Output()
+        if err != nil {
+            fmt.Fprintf(w, "Error: %q\n", err)
+        } else {
+            w.Write(data)
+        }
+    } else {
+        data, err := ioutil.ReadFile(path)
+        if err != nil {
+            fmt.Fprintf(w, "Error: %q\n", err)
+        } else {
+            w.Write(data)
+        }
     }
 }
 
 func main() {
+    fmt.Println("Reading ./config.json")
+    readConfig()
     fmt.Println("Starting HTTP Server")
     http.HandleFunc("/", handler)
     http.ListenAndServe(":8080", nil)
