1fbbcb7c5cfa86d210bae50c223034ddae833199
Jadon Fowler <jadonflower@gmail.com>
2016-05-16 18:42:07 -0700

    Add logger and clean up HTTP handler
    The logger now prints pretty messages to os.Stdout and the handler()
    function was made cleaner, though it could be better.
    
    Signed-off-by: Jadon Fowler <jadonflower@gmail.com>

--- omgos.go
+++ omgos.go
@@ -3,6 +3,7 @@ package main
 import (
     "os"
     "fmt"
+    "log"
     "strings"
     "os/exec"
     "net/http"
@@ -10,16 +11,17 @@ import (
     "encoding/json"
 )
 
+var Info *log.Logger
 var config map[string]interface{}
 
 func readConfig() {
     data, err := ioutil.ReadFile("config.json")
     if err != nil {
-        fmt.Println("  Error:", err);
+        Info.Println("Error:", err);
     } else {
         var c map[string]interface{}
         if err = json.Unmarshal(data, &c); err != nil {
-            fmt.Println("  Error:", err);
+            Info.Println("Error:", err);
             return
         }
         config = c
@@ -28,39 +30,36 @@ func readConfig() {
 
 func handler(w http.ResponseWriter, r *http.Request) {
     path := r.URL.Path[1:]
-    fmt.Printf("GET %s\n", path)
+    Info.Printf("GET %s\n", path)
     
     //Get file extension
     s := strings.Split(path, ".")
     extension := s[len(s) - 1]
     
-    //TODO: Clean up code
+    var data []u8
+    var err Error
     if(config[extension] != nil) {
         command := config[extension].(string)
-        pwd, _ := os.Getwd()
-        command = strings.Replace(command, "$file", pwd + "/" + path, -1)
-        fmt.Println(" Running command:", command)
+        command = strings.Replace(command, "$file", path, -1)
+        Info.Println("Running command:", command)
         splitCommand := strings.Split(command, " ")
         data, err := exec.Command(splitCommand[0], splitCommand[1:]...).Output()
-        if err != nil {
-            fmt.Fprintf(w, "Error: %q\n", err)
-        } else {
-            w.Write(data)
-        }
     } else {
         data, err := ioutil.ReadFile(path)
-        if err != nil {
-            fmt.Fprintf(w, "Error: %q\n", err)
-        } else {
-            w.Write(data)
-        }
+    }
+    
+    if err != nil {
+        fmt.Fprintf(w, "Error: %q\n", err)
+    } else {
+        w.Write(data)
     }
 }
 
 func main() {
-    fmt.Println("Reading ./config.json")
+    Info = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+    Info.Println("Reading ./config.json")
     readConfig()
-    fmt.Println("Starting HTTP Server")
+    Info.Println("Starting HTTP Server")
     http.HandleFunc("/", handler)
     http.ListenAndServe(":8080", nil)
 }
