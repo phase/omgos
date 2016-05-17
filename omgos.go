08f5747649df5d5fb15caca00b7febd3ea152a2e
Jadon Fowler <jadonflower@gmail.com>
2016-05-16 18:46:49 -0700

    Unexpose logger
    The logger was exposed, we don't want that.
    A syntax quirk was also fixed.
    
    Signed-off-by: Jadon Fowler <jadonflower@gmail.com>

--- omgos.go
+++ omgos.go
@@ -11,17 +11,17 @@ import (
     "encoding/json"
 )
 
-var Info *log.Logger
+var info *log.Logger
 var config map[string]interface{}
 
 func readConfig() {
     data, err := ioutil.ReadFile("config.json")
     if err != nil {
-        Info.Println("Error:", err);
+        info.Println("Error:", err);
     } else {
         var c map[string]interface{}
         if err = json.Unmarshal(data, &c); err != nil {
-            Info.Println("Error:", err);
+            info.Println("Error:", err);
             return
         }
         config = c
@@ -30,7 +30,7 @@ func readConfig() {
 
 func handler(w http.ResponseWriter, r *http.Request) {
     path := r.URL.Path[1:]
-    Info.Printf("GET %s\n", path)
+    info.Printf("GET %s\n", path)
     
     //Get file extension
     s := strings.Split(path, ".")
@@ -38,10 +38,10 @@ func handler(w http.ResponseWriter, r *http.Request) {
     
     var data []u8
     var err Error
-    if(config[extension] != nil) {
+    if config[extension] != nil {
         command := config[extension].(string)
         command = strings.Replace(command, "$file", path, -1)
-        Info.Println("Running command:", command)
+        info.Println("Running command:", command)
         splitCommand := strings.Split(command, " ")
         data, err := exec.Command(splitCommand[0], splitCommand[1:]...).Output()
     } else {
@@ -56,10 +56,10 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-    Info = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
-    Info.Println("Reading ./config.json")
+    info = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+    info.Println("Reading ./config.json")
     readConfig()
-    Info.Println("Starting HTTP Server")
+    info.Println("Starting HTTP Server")
     http.HandleFunc("/", handler)
     http.ListenAndServe(":8080", nil)
 }
